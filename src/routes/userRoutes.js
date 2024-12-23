91e5caac972f60b0ba9027c36d525b2c2d943004
Narendar2024 <narendarkorem471@gmail.com>
2024-12-23 23:50:00 +0530

    Integrated Role Based middlewares and passed that middleware to the userRoutes and the APIs are working properly Completed Implementing RBAC.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRole = require("../middlewares/roleMiddleware");
 const router = express.Router();
 
 // Only Admin can access this router
-router.get("/admin", verifyToken, (req, res) => {
+router.get("/admin", verifyToken, authorizeRole("admin"), (req, res) => {
   res.json({ message: "Welcome Admin" });
 });
 // Both Admin and Manager can access this router
-router.get("/manager", verifyToken, (req, res) => {
-  res.json({ message: "Welcome Manager" });
-});
+router.get(
+  "/manager",
+  verifyToken,
+  authorizeRole("admin", "manager"),
+  (req, res) => {
+    res.json({ message: "Welcome Manager" });
+  }
+);
 // All can access this router
-router.get("/user", verifyToken, (req, res) => {
-  res.json({ message: "Welcome User" });
-});
+router.get(
+  "/user",
+  verifyToken,
+  authorizeRole("admin", "manager", "user"),
+  (req, res) => {
+    res.json({ message: "Welcome User" });
+  }
+);
 module.exports = router;
